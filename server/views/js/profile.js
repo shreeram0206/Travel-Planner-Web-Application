3ccf238d639c69dcfabc1657aec10f5021e06ff8
shreeram0206 <shreeramramu.06@gmail.com>
2022-12-04 14:43:42 -0800

    Expense Management UI on web app

---
+++ server/views/js/profile.js
@@ -0,0 +1,8 @@
+const profile = new Vue({
+    el: "#profile-info",
+    data: {
+        user_name: "Shreeram Narayanan  ",
+        profile_info: "Student, University of Southern California"
+    }
+});
+    
